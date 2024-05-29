9429384803149b12c15b38a733e1681384bc9781
Akumbom <akumbom5ma@gmail.com>
2024-05-29 16:01:15 +0100

    implemented searchservice data persistence with mongodb

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        public AuctionSvcHttpClient(HttpClient client, IConfiguration config)
+        {
+            _client = client;
+            _config = config;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+                .Sort(x => x.Descending(x => x.UpdatedAt))
+                .Project(x => x.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+            Console.WriteLine(lastUpdated);
+
+            return await _client.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+
+        }
+    }
+}
