eb9d31bc71765f0e025074f1f7ee076395b91443
Akumbom <akumbom5ma@gmail.com>
2024-06-07 15:18:15 +0100

    wip: containerizing services

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,11 +16,13 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
+        var localAddresses = new List<string?> {"::ffff:172.20.0.1", "127.0.0.1", "::1" };
         if(HttpContext.Connection.LocalIpAddress != null)
         {
             localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
         }
+
+        Console.WriteLine("--> Debugging Remote ip: " + HttpContext.Connection.RemoteIpAddress);
 
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
         {
