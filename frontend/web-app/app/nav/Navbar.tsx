8d9c4b0444b4316182ece04ed0a699c6cbce983b
Akumbom <akumbom5ma@gmail.com>
2024-06-08 12:25:09 +0100

    .

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Navbar() {
   console.log('Client component')
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-green-800 shadow-md'>
-        <div>Maanjong Auctions</div>
+        <div>
+            <AiOutlineCar />
+            <div>Maanjong Auctions</div>
+        </div>
         <div>Middle</div>
         <div>Right</div>
     </header>
