f749aebf19d7fc3e556b978ed27ccd79712dd25e
Akumbom <akumbom5ma@gmail.com>
2024-06-08 12:05:24 +0100

    created frontend project

---
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  console.log('Client component')
+  return (
+    <header className='sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-green-800 shadow-md'>
+        <div>Maanjong Auctions</div>
+        <div>Middle</div>
+        <div>Right</div>
+    </header>
+  )
+}
